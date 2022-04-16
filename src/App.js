6a36a4431c4f1dc66eb9894108000c950fe09d40
brendacorinna <brenda.corinna@gmail.com>
2022-04-16 14:57:05 +0700

    homework3.2

--- src/App.js
+++ src/App.js
@@ -1,83 +1,100 @@
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 const CLIENT_ID = "64df437743794f03b6582c3afeb0cec6"
 const REDIRECT_URI = "http://localhost:3000/"
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 const RESPONSE_TYPE = "token"
 
-class App extends Component {
-  state={
-    search:"",
-    data:[],
-    accessToken: window.location.hash
-    .substring(1, window.location.hash.length -1)
-    .split("&")[0]
-    .split("=")[1]
+const App = () => {
+
+  const [search, setSearch] = useState("");
+  const [selectedSong, setSelectedSong] = useState([]);
+  const [accessToken, setAccessToken] = useState("")
+  const [data, setData] = useState([])
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
   }
 
-  getSpotify=()=>{
-    fetch('https://api.spotify.com/v1/search?q=' + this.state.search + '&type=track&limit=10&access_token=' + this.state.accessToken)
-    .then(res => res.json())
-    .then(data => {
+  // const handleSelectedSong=(event)=>{
+  //   setSelectedSong([...selected],uri)
+  // }
+  // const handleDeselectedSong=(event)=>{
+  //   setSelectedSong(selected.filter((item)))
+  // }
+
+  const getSpotify = () => {
+    fetch('https://api.spotify.com/v1/search?q=' + search + '&type=track&limit=10&access_token=' + accessToken)
+
+
+      .then(res => res.json())
+      .then(data => {
         console.log(data);
-        this.setState({
-            data: data.tracks.items,
-        })
-    })
+        setData(data.tracks.items)
+      });
   }
 
-  handleChange=(event)=>{
-    this.setState({
-      search:event.target.value
-    })
-  }
-
-render() {
-  return ( 
-      <div className="App">
-        <header className="Apps-Header">
+  useEffect(() => {
+    setAccessToken(window.location.hash
+      .substring(1, window.location.hash.length - 1)
+      .split("&")[0]
+      .split("=")[1])
+   
+  }, [])
+  console.log(selectedSong)
+  return (
+    <div className="App">
+      <header className="Apps-Header">
         <h1>Cille</h1>
         <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-          <input value={this.state.search} onChange={this.handleChange}></input>
-          <button onClick={this.getSpotify}>search</button>
-          <table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Song image</th>
-                    <th>Song</th>
-                    <th>Artist</th>
-                    <th>Album</th>
-                </tr>
-            </thead>
-          <tbody>
-              {this.state.data.map((item, index)=>(
-              <tr>
-            
-                <td>{index + 1}</td>
-                  <td>
-                  <img src={item.album.images[2].url}></img>
-                  </td>
-                  <td>{item.name}</td>
-                  <td>{item.artists[0].name}</td>
-                  <td>{item.album.name}</td>
-            
+        <input value={search} onChange={handleChange}></input>
+        <button onClick={getSpotify}>search</button>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Song image</th>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Album</th>
+              <th>Action</th>
             </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr>
+
+                <td>{index + 1}</td>
+                <td>
+                  <img src={item.album.images[2].url}></img>
+                </td>
+                <td>{item.name}</td>
+                <td>{item.artists[0].name}</td>
+                <td>{item.album.name}</td>
+
+                <td>
+                  {selectedSong.includes(item.uri) ?
+                    <button onClick={() => setSelectedSong(selectedSong.filter(uri => uri !== item.uri))}>Deselect</button> 
+                    :
+                    <button onClick={() => setSelectedSong([...selectedSong, item.uri])}>Select</button>
+                    }
+                </td>
+
+              </tr>
             ))}
-           
+
           </tbody>
 
-          </table>
+        </table>
 
-        </header>
+      </header>
 
-      </div>
-    );
-  }
- 
+    </div>
+  );
 }
+
 
 export default App;
